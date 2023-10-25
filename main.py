4dbb0968baf4d4fa4d33b41973f63e88c049b759
pedrobarcelos10 <pedroviniciusbarcelos@gmail.com>
2023-10-25 10:43:01 -0300

    Alteração visual na área de login

--- main.py
+++ main.py
@@ -4,134 +4,135 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.clock import Clock
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.popup import Popup
+from kivy.uix.spinner import Spinner
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.image import Image
+from kivy.core.window import Window
+from kivy.clock import Clock
 import smtplib
 from email import encoders
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
-import imaplib      
+from kivy.lang import Builder
+
+import imaplib
 import time
+
+
+Builder.load_string('''
+<CenteredHintTextInput>:
+    padding: [0, (self.height - self.line_height) / 2]
+''')
+
+
+class CenteredHintTextInput(TextInput):
+    pass
+
 
 class LoginScreen(Screen):
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
-
-        box = BoxLayout(orientation = 'vertical')
         self.name = 'login'
 
-        
-        self.email_input = TextInput(hint_text='Enter email')
-        
-        self.password_input = TextInput(hint_text='Enter password', password=True)
-        
-        self.smtp_input = TextInput(hint_text='Enter smtp server')
-        
-        self.login_button = Button(text='Login', on_press=self.login)
+        layout = BoxLayout(orientation='vertical', padding=300, spacing=10)
 
-        
-        
+        # Adicione uma imagem de logo acima das caixas de texto
+        logo_image = Image(source='unb.png', size_hint_y=None, height=130)
 
-        box.add_widget(self.email_input)
-        box.add_widget(self.password_input)
-        box.add_widget(self.smtp_input)
-        box.add_widget(self.login_button)
-        self.add_widget(box)
+        self.email_input = CenteredHintTextInput(hint_text='Enter email', size_hint_y=None, height=50)
+        self.password_input = CenteredHintTextInput(hint_text='Enter password', password=True, size_hint_y=None, height=50)
+        self.smtp_input = CenteredHintTextInput(hint_text='Enter smtp server', size_hint_y=None, height=50)
+        button_layout = BoxLayout(orientation='horizontal', size_hint_y=None, height=40)
+        spacer_left = Label()
+        login_button = Button(text='Login', size_hint=(1, 1.1), width=100, on_press=self.login)
+        spacer_right = Label()
+
+        button_layout.add_widget(spacer_left)
+        button_layout.add_widget(login_button)
+        button_layout.add_widget(spacer_right)
+
+        layout.add_widget(logo_image)
+        layout.add_widget(self.email_input)
+        layout.add_widget(self.password_input)
+        layout.add_widget(self.smtp_input)
+        layout.add_widget(button_layout)
+
+        self.add_widget(layout)
 
     def login(self, instance):
-        
         email = self.email_input.text
         password = self.password_input.text
         smtp_server = self.smtp_input.text
         Main.email = email
         Main.smtp_server = smtp_server 
         Main.password = password
-    
 
         try:
-            
             server = smtplib.SMTP_SSL(smtp_server)
-
             server.ehlo()
-            
-
-            
             server.login(email, password)
-
-            
-            print(f'Sucesso {email}')
             self.manager.current = 'main'
-
-            
             server.quit()
-            
         except smtplib.SMTPAuthenticationError:
             
             print('Login falhou. Cheque email e senha novamente.')
         except Exception as e:
-            
-            print(f'An error occurred: {str(e)}')
+            self.show_popup(f'An error occurred: {str(e)}')
 
-
-
+    def show_popup(self, message):
+        content = BoxLayout(orientation='vertical', padding=10)
+        content.add_widget(Label(text=message))
+        popup = Popup(title='Error', content=content, size_hint=(None, None), size=(300, 200))
+        popup.open()
 
 class MailClientApp(App):
     def build(self):
         screen_manager = ScreenManager()
-
-        
         login_screen = LoginScreen(name='login')
         screen_manager.add_widget(login_screen)
-
-        
         main_screen = Main(name='main')
         screen_manager.add_widget(main_screen)
-
         email_display_screen = EmailDisplayScreen(name="email_display")
         screen_manager.add_widget(email_display_screen)
-
         return screen_manager
-
 
 class Main(Screen):
     email = "" 
     smtp_server = ""
     password = "" 
-      
+    
     def __init__(self, **kwargs):
-        
         super(Main, self).__init__(**kwargs)
         self.name = 'main' 
-        box = BoxLayout(orientation='vertical')
-
-        self.text_email = TextInput(hint_text='Escreva seu email aqui', multiline=True)
-        self.subject_input = TextInput(hint_text='Assunto', size_hint = (0.5, None), height=40)
-        self.from_input = TextInput(hint_text='De', size_hint=(0.5, None), height=30)
-        self.to_input = TextInput(hint_text='Email de Destino', size_hint=(0.5, None), height=30)
-
-        self.send_email_button = Button(text='Send Email', size_hint=(None, None), size=(200, 50),
-                                        on_release=self.send_email, pos_hint={'right': 1})
-        self.send_email_button.background_color = (0.2, 0.6, 0.8, 1)
-        self.send_email_button.color = (1, 1, 1, 1)
+        layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        
+        self.from_input = TextInput(hint_text='From')
+        self.to_input = TextInput(hint_text='To')
+        self.subject_input = TextInput(hint_text='Subject')
+        self.text_email = TextInput(hint_text='Write your email here', multiline=True)
+        
+        send_email_button = Button(text='Send Email', on_release=self.send_email)
+        
         self.success_label = Label(text="", color=(0, 1, 0, 1))
-
-        self.see_emails_button = Button(text='Ver Caixa de Entrada', on_release=self.show_emails)
-
-
-        box.add_widget(self.from_input)
-        box.add_widget(self.to_input)
-        box.add_widget(self.subject_input)
-        box.add_widget(self.text_email)
-        box.add_widget(self.send_email_button)
-        box.add_widget(self.see_emails_button)
-        box.add_widget(self.success_label)
-        self.add_widget(box)
+        see_emails_button = Button(text='View Inbox', on_release=self.show_emails)
+        
+        layout.add_widget(self.from_input)
+        layout.add_widget(self.to_input)
+        layout.add_widget(self.subject_input)
+        layout.add_widget(self.text_email)
+        layout.add_widget(send_email_button)
+        layout.add_widget(see_emails_button)
+        layout.add_widget(self.success_label)
+        self.add_widget(layout)
 
     def send_email(self, button):
         server = smtplib.SMTP_SSL(self.smtp_server)
         server.login(self.email, self.password)
-
+        
         msg = MIMEMultipart()
         origem = self.from_input.text
         destino = self.to_input.text
@@ -141,7 +142,7 @@ class Main(Screen):
         msg['Subject'] = subject
 
         msg.attach(MIMEText(self.text_email.text, 'plain'))
-
+        
         text = msg.as_string()
         server.sendmail(self.email, destino, text)
 
